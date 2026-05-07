b87215c48bba2e58646de30815e1ca2a3ead9563
Max Zakharzhevskiy <maxzz@msn.com>
2026-05-06 20:54:08 -0700

    WIP. setup ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,10 @@ import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
+    base: "",
+    server: {
+        port: 3000,
+    },
     plugins: [react(), tailwindcss()],
     resolve: {
         alias: {
