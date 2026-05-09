227ea5366d498fd9b294507058f4f8c00e2c37bf
Max Zakharzhevskiy <maxzz@msn.com>
2026-05-08 20:05:10 -0700

    WIP. shadcn. update dialog. 3

--- src/ui/shadcn/dialog.tsx
+++ src/ui/shadcn/dialog.tsx
@@ -36,27 +36,63 @@ data-closed:fade-out-0"; // original has bg-black/10
 
 function DialogOverlay({ className, ...rest }: React.ComponentProps<typeof DialogPrimitive.Overlay>) {
     return (
-        <DialogPrimitive.Overlay
-            data-slot="dialog-overlay"
-            className={cn(dialogOverlayClasses, className)}
-            {...rest}
-        />
+        <DialogPrimitive.Overlay data-slot="dialog-overlay" className={cn(dialogOverlayClasses, className)} {...rest} />
     );
 }
+
+export const DialogContentClasses = "\
+fixed left-[50%] top-[50%] translate-x-[-50%] translate-y-[-50%] z-50 \
+p-6 \
+w-full md:w-full max-w-lg \
+\
+bg-background \
+\
+data-[state=open]:animate-in \
+data-[state=open]:fade-in-0 \
+data-[state=open]:zoom-in-95 \
+data-[state=open]:slide-in-from-left-1/2 \
+data-[state=open]:slide-in-from-top-[48%] \
+\
+data-[state=closed]:animate-out \
+data-[state=closed]:fade-out-0 \
+data-[state=closed]:zoom-out-95 \
+data-[state=closed]:slide-out-to-left-1/2 \
+data-[state=closed]:slide-out-to-top-[48%] \
+\
+border sm:rounded-lg shadow-lg \
+duration-200 \
+grid gap-4";
+//original: "
+// fixed top-1/2 left-1/2 z-50 
+// grid 
+// w-full 
+// max-w-[calc(100%-2rem)] 
+// -translate-x-1/2 
+// -translate-y-1/2 
+// gap-4 
+// rounded-xl 
+// bg-popover 
+// p-4 
+// text-sm 
+// text-popover-foreground 
+// ring-1 ring-foreground/10 duration-100 
+// outline-none 
+// sm:max-w-sm 
+// data-open:animate-in 
+// data-open:fade-in-0 
+// data-open:zoom-in-95 
+// data-closed:animate-out 
+// data-closed:fade-out-0 
+// data-closed:zoom-out-95
+// "
 
 function DialogContent({ className, children, showCloseButton = true, ...rest }: React.ComponentProps<typeof DialogPrimitive.Content> & { showCloseButton?: boolean; }) {
     return (
         <DialogPortal>
             <DialogOverlay />
-            <DialogPrimitive.Content
-                data-slot="dialog-content"
-                className={cn(
-                    "fixed top-1/2 left-1/2 z-50 grid w-full max-w-[calc(100%-2rem)] -translate-x-1/2 -translate-y-1/2 gap-4 rounded-xl bg-popover p-4 text-sm text-popover-foreground ring-1 ring-foreground/10 duration-100 outline-none sm:max-w-sm data-open:animate-in data-open:fade-in-0 data-open:zoom-in-95 data-closed:animate-out data-closed:fade-out-0 data-closed:zoom-out-95",
-                    className
-                )}
-                {...rest}
-            >
+            <DialogPrimitive.Content data-slot="dialog-content" className={cn(DialogContentClasses, className)} {...rest}>
                 {children}
+
                 {showCloseButton && (
                     <DialogPrimitive.Close data-slot="dialog-close" asChild>
                         <Button
@@ -64,8 +100,7 @@ function DialogContent({ className, children, showCloseButton = true, ...rest }:
                             className="absolute top-2 right-2"
                             size="icon-sm"
                         >
-                            <XIcon
-                            />
+                            <XIcon />
                             <span className="sr-only">Close</span>
                         </Button>
                     </DialogPrimitive.Close>
@@ -77,28 +112,20 @@ function DialogContent({ className, children, showCloseButton = true, ...rest }:
 
 function DialogHeader({ className, ...rest }: React.ComponentProps<"div">) {
     return (
-        <div
-            data-slot="dialog-header"
-            className={cn("flex flex-col gap-2", className)}
-            {...rest}
-        />
+        <div data-slot="dialog-header" className={cn("flex flex-col gap-2", className)} {...rest} />
     );
 }
 
 function DialogFooter({ className, showCloseButton = false, children, ...rest }: React.ComponentProps<"div"> & { showCloseButton?: boolean; }) {
     return (
-        <div
-            data-slot="dialog-footer"
-            className={cn(
-                "-mx-4 -mb-4 flex flex-col-reverse gap-2 rounded-b-xl border-t bg-muted/50 p-4 sm:flex-row sm:justify-end",
-                className
-            )}
-            {...rest}
-        >
+        <div data-slot="dialog-footer" className={cn("-mx-4 -mb-4 flex flex-col-reverse gap-2 rounded-b-xl border-t bg-muted/50 p-4 sm:flex-row sm:justify-end", className)} {...rest}>
             {children}
+
             {showCloseButton && (
                 <DialogPrimitive.Close asChild>
-                    <Button variant="outline">Close</Button>
+                    <Button variant="outline">
+                        Close
+                    </Button>
                 </DialogPrimitive.Close>
             )}
         </div>
@@ -109,10 +136,7 @@ function DialogTitle({ className, ...rest }: React.ComponentProps<typeof DialogP
     return (
         <DialogPrimitive.Title
             data-slot="dialog-title"
-            className={cn(
-                "font-heading text-base leading-none font-medium",
-                className
-            )}
+            className={cn("font-heading text-base leading-none font-medium", className)}
             {...rest}
         />
     );
@@ -122,10 +146,7 @@ function DialogDescription({ className, ...rest }: React.ComponentProps<typeof D
     return (
         <DialogPrimitive.Description
             data-slot="dialog-description"
-            className={cn(
-                "text-sm text-muted-foreground *:[a]:underline *:[a]:underline-offset-3 *:[a]:hover:text-foreground",
-                className
-            )}
+            className={cn("text-sm text-muted-foreground *:[a]:underline *:[a]:underline-offset-3 *:[a]:hover:text-foreground", className)}
             {...rest}
         />
     );
