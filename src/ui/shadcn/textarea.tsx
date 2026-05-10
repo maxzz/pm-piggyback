d4a9e989a6bdd864788c6cb5086b06811491c6fc
Max Zakharzhevskiy <maxzz@msn.com>
2026-05-09 20:54:13 -0700

    WIP. update to shadcn textarea

--- src/ui/shadcn/textarea.tsx
+++ src/ui/shadcn/textarea.tsx
@@ -1,18 +1,45 @@
-import * as React from "react"
+import { type ComponentProps } from "react";
+import { cn } from "@/utils/classnames";
 
-import { cn } from "@/utils/classnames"
-
-function Textarea({ className, ...props }: React.ComponentProps<"textarea">) {
-  return (
-    <textarea
-      data-slot="textarea"
-      className={cn(
-        "flex field-sizing-content min-h-16 w-full rounded-lg border border-input bg-transparent px-2.5 py-2 text-base transition-colors outline-none placeholder:text-muted-foreground focus-visible:border-ring focus-visible:ring-3 focus-visible:ring-ring/50 disabled:cursor-not-allowed disabled:bg-input/50 disabled:opacity-50 aria-invalid:border-destructive aria-invalid:ring-3 aria-invalid:ring-destructive/20 md:text-sm dark:bg-input/30 dark:disabled:bg-input/80 dark:aria-invalid:border-destructive/50 dark:aria-invalid:ring-destructive/40",
-        className
-      )}
-      {...props}
-    />
-  )
+export function Textarea({ className, ...rest }: ComponentProps<"textarea">) {
+    return (
+        <textarea
+            data-slot="textarea"
+            className={cn(
+                textareaClasses,
+                className
+            )}
+            {...rest}
+        />
+    );
 }
 
-export { Textarea }
+const textareaClasses = "\
+px-2.5 py-2 w-full min-h-16 field-sizing-content \
+text-xs \
+\
+bg-transparent \
+transition-colors \
+placeholder:text-muted-foreground \
+\
+dark:bg-input/30 \
+dark:disabled:bg-input/80 \
+dark:aria-invalid:border-destructive/50 \
+dark:aria-invalid:ring-destructive/40 \
+\
+focus-visible:border-ring \
+focus-visible:ring-3 \
+focus-visible:ring-ring/50 \
+\
+disabled:cursor-not-allowed \
+disabled:bg-input/50 \
+disabled:opacity-50 \
+\
+aria-invalid:border-destructive \
+aria-invalid:ring-3 \
+aria-invalid:ring-destructive/20 \
+\
+rounded-lg \
+border border-input outline-none \
+flex";
+//text-base md:text-sm \
