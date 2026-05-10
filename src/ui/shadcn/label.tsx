fee30efd8ba4ffa38c690172554ec3b7e6c254c7
Max Zakharzhevskiy <maxzz@msn.com>
2026-05-09 19:38:24 -0700

    WIP. update to shadcn label

--- src/ui/shadcn/label.tsx
+++ src/ui/shadcn/label.tsx
@@ -1,24 +1,25 @@
-"use client"
+"use client"; // 05.09.26
+import { type ComponentProps } from "react";
+import { Label as LabelPrimitive } from "radix-ui";
 
-import * as React from "react"
-import { Label as LabelPrimitive } from "radix-ui"
+import { cn } from "@/utils/classnames";
 
-import { cn } from "@/utils/classnames"
-
-function Label({
-  className,
-  ...props
-}: React.ComponentProps<typeof LabelPrimitive.Root>) {
-  return (
-    <LabelPrimitive.Root
-      data-slot="label"
-      className={cn(
-        "flex items-center gap-2 text-sm leading-none font-medium select-none group-data-[disabled=true]:pointer-events-none group-data-[disabled=true]:opacity-50 peer-disabled:cursor-not-allowed peer-disabled:opacity-50",
-        className
-      )}
-      {...props}
-    />
-  )
+export function Label({ className, ...rest }: ComponentProps<typeof LabelPrimitive.Root>) {
+    return (
+        <LabelPrimitive.Root data-slot="label" className={cn(labelClasses, className)} {...rest} />
+    );
 }
 
-export { Label }
+const labelClasses: string = "\
+text-sm \
+font-medium \
+leading-none \
+group-data-[disabled=true]:pointer-events-none \
+group-data-[disabled=true]:opacity-50 \
+peer-disabled:cursor-not-allowed \
+peer-disabled:opacity-50 \
+select-none \
+flex \
+items-center \
+gap-2 \
+";
